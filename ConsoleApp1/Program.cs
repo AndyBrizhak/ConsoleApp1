d46b30f867f28709be6323ca1d0d398e45ce8ffe
AndyBrizhak <andrewbrizhak@gmail.com>
2018-06-17 12:38:07 +0300

    +

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -11,10 +12,33 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             //
-            var x=2.5;
-            var y = 1.5;
-            double z = x % y;
+            //var x=2.5;
+            //var y = 1.5;
+            //double z = x % y;
 
+            //var outputT = "Hello"; var flag = true;
+            ////double st = 24; startM = 4; startY = 2014;
+            //string Question = @"Хочешь
+            //научиться всему?    ";
+            //Console.WriteLine(Question);
+            //Console.ReadKey();
+
+            ////var url;
+            ////url = "http://geek.ru/course/21";
+
+            //int y, m, d;
+            //y = 2015;   m = 9;  d = 28;
+
+            //StreamWriter.out("this.Text");
+            //int geek = 9;   Console.Print(geek);
+            //Console.WriteLine("{0} + {1} = {2}", 1, 5, 10);
+            //Console.ReadKey();
+
+            //int d = 2015;   var st= "Now"; string end = "Year";   Console.Write(st + d + end);
+            //Console.ReadKey();
+
+            //string output = String.Format("{0} the best ", "Geek"); Console.WriteLine(output);
+            //Console.ReadKey();
         }
     }
 }
---
+++ Sort_Col/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sort_Col
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // необобщенная коллекция ArrayList
+            //ArrayList objectList = new ArrayList() { 1, 2, "string", 'c', 2.0f };
+
+            //object obj = 45.8;
+
+            //objectList.Add(obj);
+            //objectList.Add("string2");
+
+            //foreach (object o in objectList)
+            //{
+            //    Console.WriteLine(o);
+            //}
+            //Console.WriteLine("Общее число элементов коллекции: {0}", objectList.Count);
+            //Console.ReadKey();
+            //objectList.Sort();
+            //foreach (object o in objectList)
+            //{
+            //    Console.WriteLine(o);
+            //}
+
+            // Creates and initializes a new ArrayList.
+            ArrayList myAL = new ArrayList();
+            myAL.Add("The");
+            myAL.Add("quick");
+            myAL.Add("brown");
+            myAL.Add("fox");
+            myAL.Add("jumps");
+            myAL.Add("over");
+            myAL.Add("the");
+            myAL.Add("lazy");
+            myAL.Add("dog");
+
+            // Displays the values of the ArrayList.
+            Console.WriteLine("The ArrayList initially \n contains \t the  \\ following values:");
+            PrintValues(myAL);
+            Console.ReadKey();
+            // Sorts the values of the ArrayList.
+            myAL.Sort();
+            PrintValues(myAL);
+            Console.ReadKey();
+
+            myAL.Add("crocodile");
+            PrintValues(myAL);
+            Console.ReadKey();
+
+            myAL.Reverse();
+            PrintValues(myAL);
+            Console.ReadKey();
+        }
+
+        public static void PrintValues(IEnumerable myList)
+        {
+            foreach (Object obj in myList)
+                Console.WriteLine("   {0}", obj);
+            Console.WriteLine();
+        }
+    }
+}
