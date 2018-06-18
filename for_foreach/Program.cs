c5e28918c5fa3e8405be4c1d54b8210176c01c47
AndyBrizhak <andrewbrizhak@gmail.com>
2018-06-18 09:58:47 +0300

    +

---
+++ for_foreach/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace for_foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array=new int [5];
+            int a = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                a++;
+                array[i] = a;
+            }
+
+            foreach (int value in array)
+            {
+                a--;
+                array[value] = a;
+               
+            }
+        }
+    }
+}
---
+++ if/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace @if
+{
+    class Program
+    {
+
+        static void CalculateArea(int r)
+        {
+            return;
+            double area = r * r * Math.PI;
+            
+        }
+
+        static  string  
+
+
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 3;
+            if ( x>y ) Console.WriteLine(5); else Console.WriteLine("3");
+
+            //uint width = 100;   if(DateTime.Now.Minute>=0)&&(DateTime.Now.Minute<=30)   width = 50;
+
+            //if (!(Console.ReadLine() == ".")) { Console.WriteLine("Wait"); }
+
+            //bool flag = true && !false; string outD = ""; if (flag != true) { outD = "Data Correct"; }
+
+            //double l = Convert.ToInt32(Console.ReadLine()); if (l = 2.5) Console.WriteLine("var 1");
+            //else
+            //    Console.WriteLine("var 2");
+            //int x = 0;
+
+            //double y = CalculateArea( x);
+
+            if (x>y)
+            {
+                Console.WriteLine(5);
+                //break;
+            }
+
+        }
+    }
+}
